0e565876afb421a913891a2d9371a9a129d431d4
Ryan Chen <ryanchen@spokeo.com>
2013-07-27 16:18:21 -0700

    add linklist

--- linked-lists/001.js
+++ linked-lists/001.js
@@ -11,6 +11,88 @@
  * Assume that the list contains integer values.
  */
 
+
+// Bad: not using this, make it hard to inherant
+// Bad: since we are making a constructor here
+// Follow the convention
+function make_list (value, next) {
+	return {
+		value: value,
+		next: next
+	};
+}
+
+
+// Correct
+var List = function (value, next) {
+	this.value = value;
+	this.next = next;
+}
+
+
+// Let's do more
+List.prototype = {
+
+	// Validate list
+	checkList: function (list) {
+
+		if (list) {
+			return list.hasOwnProperty('value') && 
+				list.hasOwnProperty('next') && this.checkList(this.next);
+		}
+
+		return true;
+	},
+
+	// Push function 
+	push: function (list) {
+
+		while (list) {
+			if (list.next) {
+				this = this.next;
+				break;
+			}
+
+			if (this.checkList(list)) {
+				this.next = list;
+			}
+		}
+		
+	},
+
+	// Pop function
+	pop: function () {
+		var result;
+
+		while (this) {
+			if (this.next) {
+				if (this.next.next) {
+					this = this.next;
+					break;
+				} else {
+					result = this.next;
+					this.next = undefined;
+
+					return result;
+				}
+			}
+			result = this;
+			this = undefined;
+			return result;
+		}
+
+		return undefined;
+	}
+
+
+}
+
+
+
+
+
+
+
 /*____________________________________________________________________________*/
 
 /**
